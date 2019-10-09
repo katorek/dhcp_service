0be593161441d2b6b856501a3c37b759db385537
wojciech <wojtasj1996@gmail.com>
2019-10-09 11:20:02 +0200

    WIP

--- include/config.h
+++ include/config.h
@@ -7,10 +7,10 @@
     #define TOTAL_TIME_1 0x00
     #define TOTAL_TIME_0 0x78
 
-    #define USED_INTERFACE "wlp2s0"
+    #define USED_INTERFACE "enp0s9"
 
-    #define IP_POOL_START "192.168.56.10"
-    #define IP_POOL_END "192.168.56.20"
+    #define IP_POOL_START "10.11.1.10"
+    #define IP_POOL_END "10.11.1.20"
     // subnt mast 255.255.255.0
     #define SUBNET_1 0xff
     #define SUBNET_2 0xff
