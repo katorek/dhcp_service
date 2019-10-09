95b06974f0f44ccc866a69fb3ce8ce163913a97b
wojciech <wojtasj1996@gmail.com>
2019-10-09 10:37:13 +0200

    WIP

--- src/getIpsFunctions.c
+++ src/getIpsFunctions.c
@@ -20,3 +20,23 @@ struct in_addr getMyIpAddr()
 	memcpy(&sin, &ifr.ifr_addr, sizeof(sin));
 	return sin.sin_addr;
 }
+
+uint32_t getIPForClient() {
+	uint32_t startIpPool, endIpPool, currentIp;
+	startIpPool = inet_network(IP_POOL_START);
+	endIpPool = inet_network(IP_POOL_END);
+
+	struct in_addr addr;
+	char* strIp;
+
+	for (currentIp = startIpPool; currentIp <= endIpPool; currentIp++) {
+		addr.s_addr = ntohl(currentIp);
+		strIp = inet_ntoa(addr);
+		
+		if(checkBusyIp(strIp)) continue;
+
+		if(!checkBusyIp(strIp) && checkAvailableHost(strIp)) return addr.s_addr;
+	}
+	
+	return 0;
+}
