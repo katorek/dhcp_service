47e8d363cf967308c68560b19f4205533943fea2
Wojciech Jaronski <wojtasj1996@gmail.com>
2019-10-14 19:25:43 +0200

    WIP

--- src/initPacketHeader.c
+++ src/initPacketHeader.c
@@ -10,8 +10,8 @@ void initSettings() {
     int lenServer = strlen(SERVER_IP);
 
     settings = (struct settings*) malloc(sizeof(struct settings));
-    settings->subnetmask = malloc (4 * sizeof(int));
-    settings->serverip = malloc (4 * sizeof(int));
+    settings->subnetmask = malloc (5 * sizeof(int));
+    settings->serverip = malloc (5 * sizeof(int));
 
     char mask[lenMask];
     char serv[lenServer];
@@ -20,30 +20,17 @@ void initSettings() {
     strcpy(serv, SERVER_IP);
 
     char* ptr = strtok(mask, ".");
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < 4; i++) {
         settings->subnetmask[i] = strtol(ptr, (char**) NULL, 10);
         ptr = strtok( NULL, ".");
     }
     
     ptr = strtok(serv, ".");
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < 4; i++) {
         settings->serverip[i] = strtol(ptr, (char**) NULL, 10);
         ptr = strtok( NULL, ".");
     }
 }
-
-
-int* getServerMaskArr() {
-    printf("ALA\n");
-    if(settings == NULL) initSettings();
-    return settings->subnetmask;
-}
-
-int* getServerIpArr() {
-    if(settings == NULL) initSettings();
-    return settings->serverip;
-}
-
 
 void setSubnetMask(struct dhcp_msg *packet) { //Subnet Mast '1'
     packet->option[length]   = 0x01; // 1
@@ -150,7 +137,19 @@ void setEnd(struct dhcp_msg *packet) { // End '255'
 }
 
 void initPacketHeader(struct dhcp_msg *packet, uint8_t type) {
-    if(settings == NULL) initSettings();
+    if(settings == NULL) {
+        initSettings();
+        printf("%d %d %d %d\n%d %d %d %d\n",
+        *(settings->subnetmask+0),
+        *(settings->subnetmask+1),
+        *(settings->subnetmask+2),
+        *(settings->subnetmask+3),
+        *(settings->serverip+0),
+        *(settings->serverip+1),
+        *(settings->serverip+2),
+        *(settings->serverip+3)
+    );
+    }
     length = 0;
     memset(packet->option, 0, sizeof(packet->option));
     packet->hdr.op = BOOTREPLY;
